a85d670c9a8a75fffeb6ce4971bbed6b6c6ac9b0
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-07-02 16:00:28 +0300

    59 - React JS - profile page, ajax, api

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Header } from './components/header/Header'
 import { Music } from './components/music/Music'
 import { Navbar } from './components/navbar/Navbar'
 import { News } from './components/news/News'
-import { Profile } from './components/profile/Profile'
+import ProfileContainer from './components/profile/ProfileContainer'
 import { Settings } from './components/settings/Settings'
 import { UsersContainer } from './components/users/UsersContainer'
 
@@ -17,7 +17,7 @@ const App = () => {
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<ProfileContainer />} />
           <Route path="/dialogs/*" element={<DialogsContainer />} />
           <Route path="/users/*" element={<UsersContainer />} />
           <Route path="/music" element={<Music />} />
